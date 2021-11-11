e2773c450485b6eef6cb27de3485bbb164b5ec0e
DHK32 <ilLR4056@gmail.com>
2021-11-10 20:24:13 -0800

    Update scripts.js
    Fixed incorrect object code
    Deleted first function

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,38 @@
 // Pokemon List
-let pokemonList= [];
-let typeArray= ['grass', 'poison', 'fire', 'water', 'flying', 'electric', 'bug', 'dark'];
-
-function pokemon (name, height, type1, type2= 0){
-  this.name= name;
-  this.height= height;
-  this.types= typeArray;
-  this.typePosition1= type1;
-  this.typePosition2= type2;
-}
+let typeArray= ['Grass', 'Poison', 'Fire', 'Water', 'Flying', 'Electric', 'Bug', 'Dark'];
 
 // Create Objects to store in the array.
-  pokemonList[0]= new pokemon();
-  pokemonList[0].name= 'Fearow';
-  pokemonList[0].height= 1.2;
-  pokemonList[0].type1= 5;
+  let pokemonList1 = {
+    name: 'Fearow',
+    height: 1.2,
+    types: typeArray,
+    type1: 5
 
-  pokemonList[1]= new pokemon();
-  pokemonList[1].name= 'Rapidash';
-  pokemonList[1].height= 1.7;
-  pokemonList[1].type1= 3;
+  };
 
-  pokemonList[2]= new pokemon();
-  pokemonList[2].name= 'Sharpedo';
-  pokemonList[2].height= 1.8;
-  pokemonList[2].type1= 4;
-  pokemonList[2].type2= 8;
+  let pokemonList2 = {
+    name: 'Rapidash',
+    height: 1.7,
+    types: typeArray,
+    type1: 3
+  };
 
-  pokemonList[3]= new pokemon();
-  pokemonList[3].name= 'Yanmega';
-  pokemonList[3].height= 1.9;
-  pokemonList[3].type1= 7;
+  let pokemonList3 = {
+    name: 'Sharpedo',
+    height: 1.8,
+    types: typeArray,
+    type1: 4,
+    type2: 8
+  };
+
+  let pokemonList4 = {
+    name: 'Yanmega',
+    height: 1.9,
+    types: typeArray,
+    type1: 7
+  };
+
+let pokemonList= [pokemonList1, pokemonList2, pokemonList3, pokemonList4];
 
 function displayCards(){
   for( let i=0; i < pokemonList.length; i++){
