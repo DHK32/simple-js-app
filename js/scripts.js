5e918f2c028be4f1261f9326e21a1d60e7718fa5
DHK32 <ilLR4056@gmail.com>
2022-01-16 20:18:09 -0800

    Update scripts.js
    Updated scripts.js coding.
    
    Updated for loop to include document.write.
    Added new if coditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,49 @@
 // Pokemon List
-let typeArray= ['Grass', 'Poison', 'Fire', 'Water', 'Flying', 'Electric', 'Bug', 'Dark'];
+let typeArray= ['','Grass', 'Poison', 'Fire', 'Water', 'Flying', 'Electric', 'Bug', 'Dark'];
 
 // Create Objects to store in the array.
-  let pokemonList1 = {
+let pokemonList= [
+  {
     name: 'Fearow',
     height: 1.2,
     types: typeArray,
     type1: 5
-
-  };
-
-  let pokemonList2 = {
-    name: 'Rapidash',
+  },
+  {
+    name:'Rapidash',
     height: 1.7,
     types: typeArray,
     type1: 3
-  };
-
-  let pokemonList3 = {
+  },
+  {
     name: 'Sharpedo',
     height: 1.8,
     types: typeArray,
     type1: 4,
     type2: 8
-  };
-
-  let pokemonList4 = {
+  },
+  {
     name: 'Yanmega',
     height: 1.9,
     types: typeArray,
     type1: 7
-  };
+  }
+];
 
-let pokemonList= [pokemonList1, pokemonList2, pokemonList3, pokemonList4];
 
-function displayCards(){
+
+
   for( let i=0; i < pokemonList.length; i++){
-      console.log('Name: ' + pokemonList[i].name);
-      console.log('Height: ' + pokemonList[i].height);
-      console.log('Type: ' + pokemonList[i].types[pokemonList[i].type1]);
+      document.write("<div class='card'>");
+      document.write('<p>Name: ' + pokemonList[i].name + '</p>');
+      document.write('<p>Height: ' + pokemonList[i].height + '</p>');
+      if (pokemonList[i].height > 1.5) {
+        document.write("You're tall!")
+      }
+      document.write('<p>Type: ' + pokemonList[i].types[pokemonList[i].type1]+ '</p>');
       // If the 2nd type is not null
       if(pokemonList[i].type2 > 0) {
-        console.log('Type: ' + pokemonList[i].types[pokemonList[i].type2]);
+        document.write('Type: ' + pokemonList[i].types[pokemonList[i].type2]+ '</p>');
+        }
+        document.write("</div>");
       }
-  }
-}
-displayCards();
